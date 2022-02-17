7659beb991702c2c71d461def83f9721a1dd567c
naugtur <naugtur@gmail.com>
2022-02-17 15:42:18 +0100

    pass node core modules to loaded packages

--- test-imports/index.mjs
+++ test-imports/index.mjs
@@ -3,11 +3,16 @@ import { importLocation } from '@endo/compartment-mapper';
 
 import { scaffold } from './scaffold.mjs';
 
+const read = async (s) => Buffer.from(s === 'file:///whatever.cjs' ? 'module.exports = globalThis.leak' : '{}');
+
+const modules = {};
+for (let name of ['assert', 'fs', 'path', 'os', 'util', 'buffer', 'crypto', 'events', 'stream']) {
+  modules[name] = (await importLocation(read, 'file:///whatever.cjs', { globals: { leak: await import(name) } })).namespace
+}
+
 const { testPackages } = scaffold({
   importLocation,
   globals: { process: { env: {} } },
-  modules: {
-    builtin: true,
-  },
+  modules,
 });
 testPackages();
