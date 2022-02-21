66f8081fdf56d60465441853c029fe13f950513f
naugtur <naugtur@gmail.com>
2022-02-21 17:28:06 +0100

    fix: work around more strict package.json validation

--- test-imports/index.mjs
+++ test-imports/index.mjs
@@ -1,13 +1,13 @@
-import 'ses';
-import { importLocation } from '@endo/compartment-mapper';
+import '../../endo/packages/ses/index.js';
+import { importLocation } from '../../endo/packages/compartment-mapper/index.js';
 
 import { scaffold } from './scaffold.mjs';
 
-const read = async (s) => Buffer.from(s === 'file:///whatever.cjs' ? 'module.exports = globalThis.leak' : '{}');
+const readName = (name) => async (s) => Buffer.from(s === 'file:///whatever.cjs' ? 'module.exports = globalThis.leak' : `{"name":"${name}"}`);
 
 const modules = {};
 for (let name of ['assert', 'fs', 'path', 'os', 'util', 'buffer', 'crypto', 'events', 'stream']) {
-  modules[name] = (await importLocation(read, 'file:///whatever.cjs', { globals: { leak: await import(name) } })).namespace
+  modules[name] = (await importLocation(readName(name), 'file:///whatever.cjs', { globals: { leak: await import(name) } })).namespace
 }
 
 const { testPackages } = scaffold({
