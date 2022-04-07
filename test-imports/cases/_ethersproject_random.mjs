e6c8ff08708b784eaba7849775670904169a2212
naugtur <naugtur@gmail.com>
2022-04-07 15:51:09 +0200

    manually remove incorrect default assertions

--- test-imports/cases/_ethersproject_random.mjs
+++ test-imports/cases/_ethersproject_random.mjs
@@ -1,9 +1,8 @@
 
 import * as all from '@ethersproject/random';
-import def from '@ethersproject/random';
+// manually deleted default import - esm doesn't have one, node generaets implicit
 export const name = '@ethersproject/random';
-export const expected = {"*":{"type":"object","keys":["__esModule","default","randomBytes","shuffled"]},"default":{"type":"object","keys":["randomBytes","shuffled"]}};
+export const expected = {"*":{"type":"object","keys":["__esModule","default","randomBytes","shuffled"]}};
 export const actual = { 
     '*': { type: typeof all, keys: Object.keys(all || {}).filter(a=>a).sort() },
-    default: { type: typeof def, keys: Object.keys(def || {}).filter(a=>a).sort() }
 };
--- test-imports/cases/redux.mjs
+++ test-imports/cases/redux.mjs
@@ -1,9 +1,8 @@
 
 import * as all from 'redux';
-import def from 'redux';
+// manually deleted default import - esm doesn't have one, node generaets implicit
 export const name = 'redux';
-export const expected = {"*":{"type":"object","keys":["__DO_NOT_USE__ActionTypes","__esModule","applyMiddleware","bindActionCreators","combineReducers","compose","createStore","default"]},"default":{"type":"object","keys":["__DO_NOT_USE__ActionTypes","applyMiddleware","bindActionCreators","combineReducers","compose","createStore"]}};
+export const expected = {"*":{"type":"object","keys":["__DO_NOT_USE__ActionTypes","__esModule","applyMiddleware","bindActionCreators","combineReducers","compose","createStore","default"]}};
 export const actual = { 
     '*': { type: typeof all, keys: Object.keys(all || {}).filter(a=>a).sort() },
-    default: { type: typeof def, keys: Object.keys(def || {}).filter(a=>a).sort() }
 };
