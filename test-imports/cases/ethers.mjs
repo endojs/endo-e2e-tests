02c5c8b4406b14c27fe7313fd232fd5722756b35
naugtur <naugtur@gmail.com>
2022-04-07 15:51:09 +0200

    manually remove incorrect default assertions

--- test-imports/cases/ethers.mjs
+++ test-imports/cases/ethers.mjs
@@ -1,9 +1,8 @@
 
 import * as all from 'ethers';
-import def from 'ethers';
+// manually deleted default import - esm doesn't have one, node generaets implicit
 export const name = 'ethers';
-export const expected = {"*":{"type":"object","keys":["BaseContract","BigNumber","Contract","ContractFactory","FixedNumber","Signer","VoidSigner","Wallet","Wordlist","__esModule","constants","default","errors","ethers","getDefaultProvider","logger","providers","utils","version","wordlists"]},"default":{"type":"object","keys":["BaseContract","BigNumber","Contract","ContractFactory","FixedNumber","Signer","VoidSigner","Wallet","Wordlist","constants","errors","ethers","getDefaultProvider","logger","providers","utils","version","wordlists"]}};
+export const expected = {"*":{"type":"object","keys":["BaseContract","BigNumber","Contract","ContractFactory","FixedNumber","Signer","VoidSigner","Wallet","Wordlist","__esModule","constants","default","errors","ethers","getDefaultProvider","logger","providers","utils","version","wordlists"]}};
 export const actual = { 
     '*': { type: typeof all, keys: Object.keys(all || {}).filter(a=>a).sort() },
-    default: { type: typeof def, keys: Object.keys(def || {}).filter(a=>a).sort() }
 };
