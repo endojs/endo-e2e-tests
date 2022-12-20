1ad245c79db2c63b725f28abdfcd97f0d3f9f81e
Zbyszek Tenerowicz <naugtur@gmail.com>
2022-12-20 11:40:09 +0100

    e2e GH Actions workflow

--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 ## Real package imports E2E tests
 
 [Package imports tests readme](./test-imports/README.md)
+
+### GH Actions Workflow for running e2e tests
+
+A GH Action is set up to trigger on `repository_dispatch` coming from Endo repo to run the given ref against e2e tests and compare results with a snapshot.
+
+The same workflow can also be triggered manually in this repo.
+
+_Temporarily_ the e2e test is also hooked on all pushes. We might want to disable that.
--- test-imports/test-mjs.mjs
+++ test-imports/test-mjs.mjs
@@ -1,8 +1,8 @@
-// import 'ses';
-// import { importLocation } from '@endo/compartment-mapper';
+import 'ses';
+import { importLocation } from '@endo/compartment-mapper';
 
-import '../../endo/packages/ses/index.js';
-import { importLocation } from '../../endo/packages/compartment-mapper/index.js';
+// import '../../endo/packages/ses/index.js';
+// import { importLocation } from '../../endo/packages/compartment-mapper/index.js';
 
 import { scaffold } from './tools/scaffold.mjs';
 
